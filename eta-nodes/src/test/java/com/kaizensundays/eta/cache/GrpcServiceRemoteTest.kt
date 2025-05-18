298da68a8dee9ce5ce9d38f1d04b5296f1375611
KaizenSundays <kaizensundays@localhost>
2025-05-18 14:02:18 -0400

    [ CacheLab ] GrpcServiceRemoteTest

--- eta-nodes/src/test/java/com/kaizensundays/eta/cache/GrpcServiceRemoteTest.kt
+++ eta-nodes/src/test/java/com/kaizensundays/eta/cache/GrpcServiceRemoteTest.kt
@@ -7,7 +7,10 @@ import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.grpc.stub.StreamObserver
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
 import java.time.Duration
 
 /**
@@ -17,25 +20,26 @@ import java.time.Duration
  */
 class GrpcServiceRemoteTest {
 
-    fun getStream(request: GetRequest, channel: ManagedChannel): Flux<CacheValue> {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    private fun getStream(request: GetRequest, channel: ManagedChannel): Flux<CacheValue> {
 
         return Flux.create { sink ->
 
-            val stub = EndpointGrpc.newStub(channel)
+            EndpointGrpc.newStub(channel)
+                .getStream(request, object : StreamObserver<CacheValue> {
+                    override fun onNext(cacheValue: CacheValue) {
+                        sink.next(cacheValue)
+                    }
 
-            stub.getStream(request, object : StreamObserver<CacheValue> {
-                override fun onNext(cacheValue: CacheValue) {
-                    sink.next(cacheValue)
-                }
+                    override fun onError(t: Throwable) {
+                        sink.error(t)
+                    }
 
-                override fun onError(t: Throwable) {
-                    sink.error(t)
-                }
-
-                override fun onCompleted() {
-                    sink.complete()
-                }
-            })
+                    override fun onCompleted() {
+                        sink.complete()
+                    }
+                })
 
         }
     }
@@ -53,12 +57,13 @@ class GrpcServiceRemoteTest {
 
             getStream(request, channel)
                 .take(4)
+                .publishOn(Schedulers.parallel())
                 .doOnNext { cacheValue ->
-                    println("Received: ${cacheValue.value.toStringUtf8()}")
+                    logger.info("Received: ${cacheValue.value.toStringUtf8()}")
                 }
                 .blockLast(Duration.ofSeconds(30))
         } catch (e: Exception) {
-            println("Error: ${e.message}")
+            logger.error("Error: ${e.message}", e)
         } finally {
             channel.shutdown()
         }
