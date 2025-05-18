e3b9b25e1c7f645a7118a67cf7295c766de7f5f7
KaizenSundays <kaizensundays@localhost>
2025-05-18 13:41:17 -0400

    [ CacheLab ] GrpcServiceRemoteTest

--- eta-nodes/src/test/java/com/kaizensundays/eta/cache/GrpcServiceRemoteTest.kt
+++ eta-nodes/src/test/java/com/kaizensundays/eta/cache/GrpcServiceRemoteTest.kt
@@ -4,7 +4,10 @@ import com.kaizensundays.eta.grpc.EndpointGrpc
 import com.kaizensundays.eta.grpc.GetRequest
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import io.grpc.stub.StreamObserver
 import org.junit.jupiter.api.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 /**
  * Created: Sunday 5/18/2025, 1:29 PM Eastern Time
@@ -21,15 +24,29 @@ class GrpcServiceRemoteTest {
             .usePlaintext()
             .build()
 
-        val stub = EndpointGrpc.newBlockingStub(channel)
+        val stub = EndpointGrpc.newStub(channel)
 
         try {
+            val latch = CountDownLatch(1)
+
             val request = GetRequest.newBuilder().build()
 
-            stub.getStream(request).forEach { cacheValue ->
-                println("Received: ${cacheValue.value.toStringUtf8()}")
-            }
-            println("Stream completed")
+            stub.getStream(request, object : StreamObserver<com.kaizensundays.eta.grpc.CacheValue> {
+                override fun onNext(cacheValue: com.kaizensundays.eta.grpc.CacheValue) {
+                    println("Received: ${cacheValue.value}")
+                }
+
+                override fun onError(t: Throwable) {
+                    println("Error consuming stream: ${t.message}")
+                }
+
+                override fun onCompleted() {
+                    println("Stream completed")
+                    latch.countDown()
+                }
+            })
+
+            latch.await(30, TimeUnit.SECONDS)
         } catch (e: Exception) {
             println("Error consuming stream: ${e.message}")
         } finally {
